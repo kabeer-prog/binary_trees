e056d95172eb9f2f5f3102255613fe2c48458748
kabeer-prog <codekabeer@DESKTOP-GO7PRNU.localdomain>
2022-08-25 14:21:37 +0100

    insert left task

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,29 @@
+#include "binary_trees.h"
+
+
+/**
+ * binary_tree_node -Function that creates a binary tree node
+ *
+ * @parent: Pointer to the root node of the tree
+ * @value: Value of the node
+ *
+ * Return: Pointer to the new node
+ */
+
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new_node = NULL;
+
+	new_node = malloc(sizeof(binary_tree_t));
+
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->n = value;
+	new_node->parent = parent;
+	new_node->left = NULL;
+	new_node->right = NULL;
+
+	return (new_node);
+
+}
